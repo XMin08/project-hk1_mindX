c858142ace19b5ce3e9596a16f2eda6b859ab4d1
XMin08 <xudacxuanminh0220@gmail.com>
2023-03-08 00:04:44 +0700

    complete detail

--- product.js
+++ product.js
@@ -1,4 +1,4 @@
-
+let ID_product = 1;
 
 function createDivChild(x){
     // const x = document.getElementById("test");
@@ -8,7 +8,8 @@ function createDivChild(x){
     if(div === null || div.tagName !== "DIV")
         check = false;
     
-        if(icon.className === "fa-solid fa-caret-down"){
+    if(icon.className === "fa-solid fa-caret-down"){
+        
         icon.className = "fa-solid fa-caret-up";
         if(check){
             div.classList.add("menu_active");
@@ -25,12 +26,13 @@ function createDivChild(x){
 }
 
 
-const parent = document.getElementById("product");
+
 async function getProduct(index){
     
     const api = "http://127.0.0.1:5500/list_product.json"
     const res = await fetch(api);
     const data = await res.json();
+    const parent = document.getElementById("product");
 
     var len = index + 9;
     if(len > data.length){
@@ -40,13 +42,13 @@ async function getProduct(index){
     for(let i = index; i < len; i++){
         const product = data[i];
 
-        const {img , name, cost} = product;
+        const {img , name, cost, id} = product;
         const product_element = document.createElement("div");
-
+        
         product_element.classList.add("detail");
 
         const product_ele_content = `
-        <a href="###">
+        <a href="./detail.html" id="${id}" onload = "assignID(${id})">
             <div class="pic">
                 <img src=${img}
                 alt="picture">
@@ -119,7 +121,7 @@ function switchPageNumber(number){
         }
     }
 }
-switchPageNumber(0);
+
 
 function switchByButton(name){
     
@@ -130,4 +132,101 @@ function switchByButton(name){
     if(name === "right" && page_number < max_page_number){
         switchPageNumber(page_number + 1);
     }
+}
+
+function assignID(id){
+    ID_product = id;
+}
+
+async function getDetail(){
+    const api = "http://127.0.0.1:5500/list_product.json"
+    const res = await fetch(api);
+    const data = await res.json();
+    console.log(ID_product);
+    let parent = document.getElementById("detail");
+
+    const product = data[ID_product - 1];
+    console.log(product)
+
+    const {img , name, cost} = product;
+    const img_element = document.createElement("div");
+    img_element.classList.add("img_product");
+    
+    const img_element_content = `<img src="${img}" alt="picture">`;
+
+   img_element.innerHTML = img_element_content;
+   parent.insertBefore(img_element,document.getElementById("infor"));
+
+
+   parent = document.getElementById("infor");
+   const title_element = document.createElement("div");
+   title_element.classList.add("name_title");
+   const title_element_content = `
+   <h1>${name}</h1>
+   <span>${cost}</span>
+   <i class="fa-thin fa-dollar-sign"></i>`;
+
+   title_element.innerHTML = title_element_content;
+   parent.insertBefore(title_element,document.getElementById("size"));
+
+
+   parent = document.getElementById("infor");
+   const ID_element = document.createElement("div");
+   ID_element.classList.add("ID_product");
+   const ID_element_content = `
+   <p>Mã sản phẩm</p>
+    <span>${ID_product}</span>`;
+
+   ID_element.innerHTML = ID_element_content;
+   parent.insertBefore(ID_element,document.getElementById("size"));
+}
+
+function createDivText(x){
+    // const x = document.getElementById("test");
+    var icon = x.children[1];
+    const div = x.nextElementSibling;
+    let check = true;
+    if(div === null || div.tagName !== "DIV")
+        check = false;
+    
+    if(icon.className === "fa-solid fa-plus"){
+        
+        icon.className = "fa-solid fa-minus";
+        if(check){
+            div.classList.add("text_active");
+        }
+    }
+    else{
+
+        icon.className = "fa-solid fa-plus";
+        if(check){
+            div.classList.remove("text_active");
+        }
+            
+    }
+    
+}
+
+function changeValueAmount(check){
+    const parent = document.getElementById("amount");
+    var input = parent.children[1];
+    if(check === "minus" && input.value > 1){
+        input.value = Number(input.value) - 1;
+    }
+    if(check === "plus"){
+        input.value = Number(input.value) + 1;
+    }
+}
+
+function fixSize(li){
+
+    const list = document.getElementById("list_size");
+    console.log(list.children.length);
+    for(let i = 0; i < list.children.length; i++){
+        if(list.children[i].className === "size_active"){
+            list.children[i].classList.remove("size_active");
+        }
+    }
+
+    li.classList.add("size_active");
 }

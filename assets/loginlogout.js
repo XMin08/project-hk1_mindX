77b341be4f08816ccf51de45f1964727e4fc9960
XMin08 <xudacxuanminh0220@gmail.com>
2023-03-12 16:20:09 +0700

    update loginlogout

--- assets/loginlogout.js
+++ assets/loginlogout.js
@@ -16,8 +16,8 @@ function login(){
     const user_local = localStorage.getItem("account_name");
     const pass_local = localStorage.getItem("account_password");
 
-    const user_session = localStorage.getItem("account_name");
-    const pass_session = localStorage.getItem("account_password");
+    const user_session = sessionStorage.getItem("account_name");
+    const pass_session = sessionStorage.getItem("account_password");
 
     const user_login = document.getElementById("email_login").value;
     const pass_login = document.getElementById("password_login").value;
@@ -34,6 +34,29 @@ function login(){
         return;
     }
     alert("Wrong email or password");
+}
+
+function register(){
+
+    const name = String(document.getElementById("name_reg").value);
+    const email = String(document.getElementById("email_reg").value);
+    const pass = String(document.getElementById("pass_reg").value);
+    const re_pass = String(document.getElementById("re_pass_reg").value);
+
+    console.log(email, email.indexOf("@"), email.lastIndexOf("."));
+    if(email.indexOf("@") < 1 || email.indexOf("@") + 2 > email.lastIndexOf(".") || email.lastIndexOf(".") + 2 >= email.length){
+        alert("Wrong email");
+        return;
+    }
+    if(pass !== re_pass || pass === ""){
+        alert("Password wrong");
+        return;
+    }
+
+    setLogin();
+    sessionStorage.setItem("account_name", email);
+    sessionStorage.setItem("account_password", pass);
+    window.location = "http://127.0.0.1:5500/assets/loginlogout.html";
 }
 
 function setLogin(){
